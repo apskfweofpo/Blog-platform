998418c0311dcd4f7e2281a89b8a9a7e4c80c39d
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-18 18:04:37 +0300

    change forRoot

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,6 +5,7 @@ import {databaseConfig} from "./config/databaseConfig";
 import {UsersService} from "./users/users.service";
 import {TypeOrmConfigService} from "./config/typeOrmConfigService"
 import {UsersController} from "./users/users.controller";
+import {User} from "./users/users.model";
 
 @Module({
     imports: [
@@ -13,8 +14,15 @@ import {UsersController} from "./users/users.controller";
             envFilePath: `${process.cwd()}/config/env/${process.env.NODE_ENV}.env`,
             load: [databaseConfig]
         }),
-        TypeOrmModule.forRootAsync({
-            useClass: TypeOrmConfigService
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DATABASE_HOST || 'localhost',
+            port: parseInt( process.env.DATABASE_PORT) || 5432,
+            username: process.env.DATABASE_USER || 'postgres',
+            password: process.env.DATABASE_PASSWORD || 'carrynum1',
+            database: process.env.DATABASE_NAME || 'blog-platform',
+            synchronize: true,
+            entities: [User]
         }),
     ],
     controllers: [UsersController],
