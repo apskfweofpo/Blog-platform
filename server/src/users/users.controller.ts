a59310aae5cc6ff6d5d8ecea2920359a39139acf
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-18 18:05:47 +0300

    add post signup

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Header, HttpCode, HttpStatus, Injectable, Post} from '@nestjs/common';
+import {UsersService} from "./users.service";
 
+@Injectable()
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(
+        private readonly userService: UsersService) {
+    }
+
+    @Post('/signup')
+    @HttpCode(HttpStatus.CREATED)
+    @Header('Content-type', 'application/json')
+    createUser(@Body() createUserDto) {
+        return this.userService.createUser(createUserDto);
+    }
+}
