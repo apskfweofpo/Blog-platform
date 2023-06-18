eb104693042b70ebe924daed6cec1ab6c224f78b
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-18 11:52:09 +0300

    create config for typeOrm

---
+++ server/src/config/typeOrmConfigService.ts
@@ -0,0 +1,28 @@
+import {TypeOrmModuleOptions, TypeOrmOptionsFactory} from "@nestjs/typeorm";
+import {Injectable} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
+
+@Injectable()
+export class TypeOrmConfigService  implements TypeOrmOptionsFactory {
+    constructor(private readonly configService: ConfigService) {
+    }
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+
+        const {
+            sql: { type,
+                host,port,username,password,
+                database,synchronize,migrations},
+        } = this.configService.get('database');
+
+        return {
+            type,
+            host,
+            port,
+            username,
+            password,
+            database,
+            synchronize,
+            migrations,
+        };
+    }
+}
