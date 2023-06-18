177c772efde267db139d28cbeb86d620db79b6ad
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-18 18:04:56 +0300

    add createUser

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import {InjectEntityManager, InjectRepository} from "@nestjs/typeorm";
+import {User} from "./users.model";
+import {Repository} from "typeorm";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+    ) {}
+
+    async createUser(createUserDto) {
+        const user = User.create();
+        user.username = createUserDto.username;
+        user.email = createUserDto.email;
+        user.password = createUserDto.password;
+        return User.save(user);
+
+    }
+}
