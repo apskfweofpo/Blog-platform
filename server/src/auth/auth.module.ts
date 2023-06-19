0e0fc7e0ff8dd16ac3b01f05db9cceba9cc9787c
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-19 17:52:34 +0300

    add jwtModule

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import {UsersService} from "../users/users.service";
 import {UsersModule} from "../users/users.module";
+import {PassportModule} from "@nestjs/passport";
+import {LocalStrategy} from "./strategies/local.strategy";
+import {JwtModule} from "@nestjs/jwt";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {JwtStrategy} from "./strategies/jwt.strategy";
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AuthController],
-  providers: [AuthService]
+  imports: [UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {expiresIn: '33d'}
+        }),
+        inject: [ConfigService]
+    }),
+  ],
+    providers: [AuthService, LocalStrategy, JwtStrategy],
+    controllers: [AuthController],
 })
 export class AuthModule {}
