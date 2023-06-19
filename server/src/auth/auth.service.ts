d1c1c3edc6a227e6d54ea3b962972a0bbd6ee973
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-19 13:35:43 +0300

    add validate User

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
+import {Repository} from "typeorm";
+import {User} from "../users/users.model";
+import {UsersService} from "../users/users.service";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        private readonly userService: UsersService
+    ) {
+    }
+
+    async validateUser(email: string, password: string): Promise<any> {
+        const user = await this.userService.findOne(email);
+        const passwordIsValid = await bcrypt.compare(user.password, password)
+        if (user &&  passwordIsValid) {
+            const {password, ...result} = user;
+            return result;
+        }
+        throw new BadRequestException('User or password are incorrect')
+    }
+}
