5da9425fdec9805dfd800d76bc464150e7bbf82e
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-20 16:58:24 +0300

    add jwt module

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,13 +1,26 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import {Module} from '@nestjs/common';
+import {UsersController} from './users.controller';
+import {UsersService} from './users.service';
 import {User} from "./users.model";
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {JwtModule} from "@nestjs/jwt";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService]
+    imports: [
+        TypeOrmModule.forFeature([User]),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                secret: configService.get('JWT_SECRET'),
+                signOptions: {expiresIn: '33d'}
+            }),
+            inject: [ConfigService]
+        }),
+    ],
+    controllers: [UsersController],
+    providers: [UsersService],
+    exports: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule {
+}
