68d99652600ccf87906ff5f7fc9aaf8b9bb822df
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-24 01:41:24 +0300

    add create method

--- server/src/blog/blog.service.ts
+++ server/src/blog/blog.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import {CreateBlogDto} from "./dto/CreateBlogDto";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {Blog} from "./blog.model";
+import {FilesService} from "../files/files.service";
+import {User} from "../users/users.model";
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+    constructor(
+        @InjectRepository(Blog)
+        private readonly blogRepository: Repository<Blog>,
+
+        @InjectRepository(Blog)
+        private readonly userRepository: Repository<User>,
+
+        private readonly fileService: FilesService
+    ) {
+    }
+    async create(blog: CreateBlogDto, id: number, image: Express.Multer.File): Promise<Blog> {
+        const fileName = await this.fileService.createFile(image);
+
+
+        const author = await this.userRepository.findOne({
+            where: {
+                id
+            }
+        })
+
+
+        const newBlog = this.blogRepository.create();
+        newBlog.title = blog.title;
+        newBlog.content = blog.content;
+        newBlog.image = fileName;
+        newBlog.author = author;
+
+
+        return await this.blogRepository.save(newBlog);
+
+
+    }
+}
