179c0627b307f721fa49298ad0b99aa944fab556
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-24 01:40:50 +0300

    add file inter

--- server/src/blog/blog.controller.ts
+++ server/src/blog/blog.controller.ts
@@ -1,8 +1,8 @@
-import {Body, Controller, Post, UploadedFile, UseGuards, Request} from '@nestjs/common';
-import {AuthService} from "../auth/auth.service";
+import {Body, Controller, Post, UploadedFile, UseGuards, Request, UseInterceptors} from '@nestjs/common';
 import {BlogService} from "./blog.service";
 import {CreateBlogDto} from "./dto/CreateBlogDto";
 import {JwtAuthGuard} from "../guards/jwt-auth.guard";
+import {FileInterceptor} from "@nestjs/platform-express";
 
 @Controller('blog')
 export class BlogController {
@@ -13,12 +13,11 @@ export class BlogController {
 
     @Post('create')
     @UseGuards(JwtAuthGuard)
+    @UseInterceptors(FileInterceptor('image'))
     createPost(@Request() req,
                @Body() blog: CreateBlogDto,
-               @UploadedFile() image,) {
-        this.blogService.create(blog, req.user.id, image);
-
-        //console.log(req.user.id)
+               @UploadedFile() image: Express.Multer.File,) {
+       return  this.blogService.create(blog, req.user.id, image);
     }
 
 }
