d56c521e426c0d00051b6d1c86824c530ead6fc2
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-24 00:58:35 +0300

    add post req for add blog

--- server/src/blog/blog.controller.ts
+++ server/src/blog/blog.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post, UploadedFile, UseGuards, Request} from '@nestjs/common';
+import {AuthService} from "../auth/auth.service";
+import {BlogService} from "./blog.service";
+import {CreateBlogDto} from "./dto/CreateBlogDto";
+import {JwtAuthGuard} from "../guards/jwt-auth.guard";
 
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+    constructor(
+        private readonly blogService: BlogService
+    ) {
+    }
+
+    @Post('create')
+    @UseGuards(JwtAuthGuard)
+    createPost(@Request() req,
+               @Body() blog: CreateBlogDto,
+               @UploadedFile() image,) {
+        this.blogService.create(blog, req.user.id, image);
+
+        //console.log(req.user.id)
+    }
+
+}
