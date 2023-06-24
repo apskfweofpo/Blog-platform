321cd001b9427e79545ccee88550eb4b64e2a061
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-25 00:24:01 +0300

    change validators for params

--- server/src/blog/dto/UpdateBlogDto.ts
+++ server/src/blog/dto/UpdateBlogDto.ts
@@ -1,18 +1,16 @@
-import {IsNotEmpty} from "class-validator";
+import {IsNotEmpty, IsOptional} from "class-validator";
 import {ApiProperty} from "@nestjs/swagger";
 
 export class UpdateBlogDto {
     @ApiProperty({example: 'Animals'})
-    @IsNotEmpty()
+    @IsOptional()
     readonly title: string
 
     @ApiProperty({example: '66914a6e-62a2-4f0d-99a0-3fc975bb549f.jpg'})
-    @IsNotEmpty()
+    @IsOptional()
     readonly image: string
 
     @ApiProperty({example: 'There are millions animals.'})
-    @IsNotEmpty()
+    @IsOptional()
     readonly content: string
-
-
 }
