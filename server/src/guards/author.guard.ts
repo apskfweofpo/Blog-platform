0c9ca163e62e8768e1a9c65f53032bd57307867f
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-24 19:07:14 +0300

    add validation for authors or admins

---
+++ server/src/guards/author.guard.ts
@@ -0,0 +1,39 @@
+import {CanActivate, ExecutionContext} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {User} from "../users/users.model";
+import {Repository} from "typeorm";
+import {Blog} from "../blog/blog.model";
+
+export class AuthorGuard implements CanActivate {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+        @InjectRepository(Blog)
+        private readonly blogRepository: Repository<Blog>,
+    ) {
+    }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+
+        const request = context.switchToHttp().getRequest();
+        const {id} = request.params;
+
+        const blog = await this.blogRepository.findOne({
+            where: {id}
+        })
+
+        const authorId = blog.author.id;
+
+        const userId = request.user.id;
+
+        const user = await this.userRepository.findOne({
+            where: {
+                id: userId
+            }
+        })
+
+        if (blog && userId && authorId === userId || user.role === "admin") {
+            return true;
+        } else false;
+    }
+}
