3a1e4cc1a0c41490004d0bc32ca479a4ba35c2b1
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-24 19:07:46 +0300

    add get All and get one and update

--- server/src/blog/blog.controller.ts
+++ server/src/blog/blog.controller.ts
@@ -6,13 +6,14 @@ import {
     UseGuards,
     Request,
     UseInterceptors,
-    NestInterceptor
+    NestInterceptor, Get, Param, Patch
 } from '@nestjs/common';
 import {BlogService} from "./blog.service";
 import {CreateBlogDto} from "./dto/CreateBlogDto";
 import {JwtAuthGuard} from "../guards/jwt-auth.guard";
 import {FileInterceptor} from "@nestjs/platform-express";
 import {Blog} from "./blog.model";
+import {UpdateBlogDto} from "./dto/UpdateBlogDto";
 
 @Controller('blog')
 export class BlogController {
@@ -24,10 +25,29 @@ export class BlogController {
     @Post('create')
     @UseInterceptors(FileInterceptor('image'))
     @UseGuards(JwtAuthGuard)
-    async createPost(@Request() req,
-               @Body() blog: CreateBlogDto,
-               @UploadedFile() image: Express.Multer.File,): Promise<Blog> {
+    async createBlog(@Request() req,
+                     @Body() blog: CreateBlogDto,
+                     @UploadedFile() image: Express.Multer.File): Promise<Blog> {
         return this.blogService.create(blog, req.user.id, image);
     }
 
+    @Get('all')
+    async getBlogs() {
+        return this.blogService.getAll();
+    }
+
+    @Get('find/:id')
+    async getBlog(@Param('id') id: number) {
+        return this.blogService.getOne(id);
+    }
+
+
+    @Patch('update:id')
+    updateBlog(
+        @Param('id') blogId: number,
+        @Body() blog: UpdateBlogDto,
+        @UploadedFile() image: Express.Multer.File
+    ) {
+        return this.blogService.update(blog, blogId, image);
+    }
 }
