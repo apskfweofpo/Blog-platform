8460b5cf4b8ea131afecb4336a92b139c34b1275
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-26 01:11:57 +0300

    add role

--- server/src/auth/strategies/jwt.strategy.ts
+++ server/src/auth/strategies/jwt.strategy.ts
@@ -17,6 +17,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(user: IUser) {
-        return { id: user.id, email: user.email };
+        return { id: user.id, email: user.email, role: user.role };
     }
 }
