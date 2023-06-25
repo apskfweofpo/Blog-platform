cac53b01ab23c7cc450bc6d554cd905d9124ef5c
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-26 02:13:16 +0300

    add create getAll and remove

--- server/src/categories/categories.service.ts
+++ server/src/categories/categories.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Blog} from "../blog/blog.model";
+import {Repository} from "typeorm";
+import {Category} from "./category.model";
+import {CreateCategoryDto} from "./dto/createCategoryDto";
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+    constructor(
+        @InjectRepository(Category)
+        private readonly categoryRepository: Repository<Category>,
+    ) {
+    }
+
+    createCategory(category: CreateCategoryDto): Promise<Category> {
+        return  this.categoryRepository.save(category)
+    }
+
+    getAll() : Promise<Category[]> {
+        return this.categoryRepository.find()
+    }
+
+    async remove(id: number) {
+        console.log(id)
+        const category = await this.categoryRepository.findOne({
+            where: {id}
+        })
+        if(!category){
+            throw new NotFoundException(`Category with id ${id} not found`)
+        }
+
+        return this.categoryRepository.remove(category)
+    }
+}
