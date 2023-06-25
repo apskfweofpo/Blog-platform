e49d35fa583f7d78b524917a8e2bbfbdc9a91404
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-26 02:13:49 +0300

    add create remove and getAll

--- server/src/categories/categories.controller.ts
+++ server/src/categories/categories.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post} from '@nestjs/common';
+import {CategoriesService} from "./categories.service";
+import {CreateCategoryDto} from "./dto/createCategoryDto";
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+    constructor(
+        private readonly categoriesService: CategoriesService
+    ) {
+    }
+
+    @Post('create')
+    addOne(
+        @Body() category: CreateCategoryDto
+    ) {
+        return this.categoriesService.createCategory(category)
+    }
+
+    @Get('')
+    getAll() {
+        return this.categoriesService.getAll()
+    }
+
+    @Delete('/:id')
+    removeCategory(
+        @Param('id') id: number
+    ) {
+        return  this.categoriesService.remove(id)
+    }
+}
