a13d5becd3688d5b95e33d123b88acda1e8e7517
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-27 01:00:36 +0300

    add apiBody and api ok res

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import {Controller, Post, UseGuards, Request, Get} from '@nestjs/common';
+import {Controller, Post, UseGuards, Request, Get, HttpCode, HttpStatus, Header} from '@nestjs/common';
 import {AuthService} from "./auth.service";
 import {LocalAuthGuard} from "../guards/local-auth.guard";
 import {JwtAuthGuard} from "../guards/jwt-auth.guard";
 import {Roles} from "../decorators/roles.decorator";
 import {RolesGuard} from "../guards/roles.guard";
+import {ApiBody, ApiOkResponse} from "@nestjs/swagger";
+import {LoginUserRequest, LoginUserResponse} from "./types";
 
 @Controller('auth')
 export class AuthController {
@@ -12,8 +14,11 @@ export class AuthController {
     ) {
     }
 
-    @UseGuards(LocalAuthGuard)
+    @ApiBody({ type: LoginUserRequest })
+    @ApiOkResponse({ type: LoginUserResponse })
     @Post('login')
+    @UseGuards(LocalAuthGuard)
+    @HttpCode(HttpStatus.OK)
     async login(@Request() req) {
         return this.authService.login(req.user);
     }
