000a31a0918848e889a1dd23ad086a2bda785d33
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-27 00:13:23 +0300

    add swagger doc

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import {Logger} from "@nestjs/common";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  Logger.log(`Server start on port: ${ process.env.PORT}`)
-  await app.listen(process.env.PORT);
+    const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle('Blog platform')
+        .setDescription('Api documentation')
+        .setVersion('0.3')
+        .addTag('api')
+        .build();
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('swagger', app, document)
+    Logger.log(`Server start on port: ${process.env.PORT}`)
+    await app.listen(process.env.PORT);
 }
+
 bootstrap();
