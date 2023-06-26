81d3ad2d19004372a27a2540bb99d6c1cd33dda3
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-26 16:47:26 +0300

    create types for response

---
+++ server/src/users/types/index.ts
@@ -0,0 +1,52 @@
+import {ApiProperty} from "@nestjs/swagger";
+
+export class SignupResponse {
+    @ApiProperty({example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAsImVtYWlsIjoiYWRtaW4xQG1haWwucnUiLCJ1c2VybmFtZSI6ImFkbWluMSIsInJvbGUiOiJjbGllbnQiLCJpYXQiOjE2ODc3ODY0NTcsImV4cCI6MTY5MDYzNzY1N30.qAfvUyvgfCzEjJc0gXX7rU7Byr2ny8ukuDK3V4HYKA4'})
+    access_token: string;
+
+    @ApiProperty({example: 'admin1@mail.ru'})
+    email: string;
+
+    @ApiProperty({example: 'admin1'})
+    username: string;
+
+    @ApiProperty({example: 'client'})
+    role: string;
+}
+
+
+class Category {
+    @ApiProperty({example: '1'})
+    id: number;
+
+    @ApiProperty({example: 'Programming'})
+    name: string;
+}
+
+class Blog {
+    @ApiProperty({example: '19'})
+    id: number;
+
+    @ApiProperty({example: 'Programming'})
+    title: string;
+
+    @ApiProperty({example: 'a1c528c1-741d-4a2e-8ac8-7305297848d5.jpg'})
+    image: string;
+
+    @ApiProperty({example: 'There are many types of programming'})
+    content: string;
+
+    @ApiProperty({type: Category, isArray: true})
+    categories: Category[];
+}
+
+export class GetUserResponse {
+    @ApiProperty({example: '19'})
+    id: string;
+
+    @ApiProperty({example: 'admin'})
+    username: string;
+
+    @ApiProperty({type: Blog, isArray: true})
+    blogs: Blog[];
+}
