988c06e65859499b1523ea861139d670113461af
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-27 01:01:00 +0300

    add api ok res

--- server/src/categories/categories.controller.ts
+++ server/src/categories/categories.controller.ts
@@ -4,6 +4,8 @@ import {CreateCategoryDto} from "./dto/createCategoryDto";
 import {JwtAuthGuard} from "../guards/jwt-auth.guard";
 import {RolesGuard} from "../guards/roles.guard";
 import {Roles} from "../decorators/roles.decorator";
+import {ApiOkResponse} from "@nestjs/swagger";
+import {Category, CreateCategoryResponse, RemoveCategoryResponse} from "./types";
 
 @Controller('categories')
 export class CategoriesController {
@@ -12,6 +14,7 @@ export class CategoriesController {
     ) {
     }
 
+    @ApiOkResponse({type: CreateCategoryResponse})
     @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles('admin')
     @Post('create')
@@ -21,17 +24,19 @@ export class CategoriesController {
         return this.categoriesService.createCategory(category)
     }
 
+    @ApiOkResponse({type: [Category]})
     @Get('')
     getAll() {
         return this.categoriesService.getAll()
     }
 
+    @ApiOkResponse({type: RemoveCategoryResponse})
     @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles('admin')
     @Delete('/:id')
     removeCategory(
         @Param('id') id: number
     ) {
-        return  this.categoriesService.remove(id)
+        return this.categoriesService.remove(id)
     }
 }
