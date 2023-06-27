5567f1551c9652bba18e6553301a2871e88863f2
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-27 13:52:54 +0300

    remove image

--- server/src/blog/dto/CreateBlogDto.ts
+++ server/src/blog/dto/CreateBlogDto.ts
@@ -6,10 +6,6 @@ export class CreateBlogDto {
     @IsNotEmpty()
     readonly title: string
 
-    @ApiProperty({example: '66914a6e-62a2-4f0d-99a0-3fc975bb549f.jpg'})
-    @IsNotEmpty()
-    readonly image: string
-
     @ApiProperty({example: 'There are millions animals.'})
     @IsNotEmpty()
     readonly content: string
