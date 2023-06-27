9de21d8fa2399cc5744e72872b4247ddfd9d22f0
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-27 13:51:36 +0300

    remove image

--- server/src/blog/dto/UpdateBlogDto.ts
+++ server/src/blog/dto/UpdateBlogDto.ts
@@ -1,14 +1,10 @@
-import {IsNotEmpty, IsOptional} from "class-validator";
+import {IsOptional} from "class-validator";
 import {ApiProperty} from "@nestjs/swagger";
 
 export class UpdateBlogDto {
     @ApiProperty({example: 'Animals'})
     @IsOptional()
     readonly title: string
-
-    @ApiProperty({example: '66914a6e-62a2-4f0d-99a0-3fc975bb549f.jpg'})
-    @IsOptional()
-    readonly image: string
 
     @ApiProperty({example: 'There are millions animals.'})
     @IsOptional()
