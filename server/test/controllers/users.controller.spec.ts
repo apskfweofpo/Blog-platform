a8eb3c0da1255221e76dce521d315c5008e7a52f
Pavel Krikunov <fusrodah.df@mail.ru>
2023-07-01 23:52:34 +0300

    add test for creating user

--- server/test/controllers/users.controller.spec.ts
+++ server/test/controllers/users.controller.spec.ts
@@ -1,24 +1,30 @@
-import {INestApplication} from "@nestjs/common";
+import {INestApplication, Logger} from "@nestjs/common";
 import {Test, TestingModule} from "@nestjs/testing";
-import {TypeOrmModule} from "@nestjs/typeorm";
+import {InjectRepository, TypeOrmModule} from "@nestjs/typeorm";
 import {User} from "../../src/users/users.model";
-import {JwtModule} from "@nestjs/jwt";
 import {ConfigModule, ConfigService} from "@nestjs/config";
+import {databaseConfig} from "../../src/config/databaseConfig";
+import {TypeOrmConfigService} from "../../src/config/typeOrmConfigService";
+import {UsersModule} from "../../src/users/users.module";
+import * as bcrypt from "bcrypt";
+import * as request from "supertest";
+import {Repository} from "typeorm";
 
 describe('Users controller', () => {
     let app: INestApplication;
     beforeEach(async () => {
         const testModule: TestingModule = await Test.createTestingModule({
             imports: [
-                TypeOrmModule.forFeature([User]),
-                JwtModule.registerAsync({
-                    imports: [ConfigModule],
-                    useFactory: (configService: ConfigService) => ({
-                        secret: configService.get('JWT_SECRET'),
-                        signOptions: {expiresIn: '33d'}
-                    }),
-                    inject: [ConfigService]
+                ConfigModule.forRoot({
+                    isGlobal: true,
+                    envFilePath: `${process.cwd()}/config/env/${process.env.NODE_ENV}.env`,
+                    load: [databaseConfig]
                 }),
+                TypeOrmModule.forRootAsync({
+                    imports: [ConfigModule],
+                    useClass: TypeOrmConfigService
+                }),
+                UsersModule,
             ],
         }).compile();
 
@@ -28,6 +34,34 @@ describe('Users controller', () => {
     })
 
     afterEach(async () => {
-        await User.destroy({where: {username: 'Test'}})
+
+
+
+    })
+
+
+    it('Should create user', async () => {
+
+        const newUser = {
+            username: 'test12345',
+            email: "test12345@gmail.com",
+            password: '12345'
+        }
+
+        const response = await request(app.getHttpServer())
+            .post('/users/signup')
+            .send(newUser);
+
+
+
+        const passwordIsValid = await bcrypt.compare(
+            newUser.password,
+            response.body.password,
+        );
+
+        expect(response.body.username).toBe(newUser.username);
+        expect(passwordIsValid).toBe(true);
+        expect(response.body.email).toBe(newUser.email);
+
     })
 })
