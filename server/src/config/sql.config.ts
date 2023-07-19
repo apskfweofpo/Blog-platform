fd5c2cf68098564a86a739269e7127a42b2f247f
Pavel Krikunov <fusrodah.df@mail.ru>
2023-07-20 00:02:16 +0300

    change env values

--- server/src/config/sql.config.ts
+++ server/src/config/sql.config.ts
@@ -3,12 +3,12 @@ import {registerAs} from '@nestjs/config';
 export const sqlConfig = registerAs('database', () => ({
     NODE_ENV: process.env.NODE_ENV,
     type: 'postgres',
-    host: process.env.DATABASE_HOST || 'localhost',
-    server_port: process.env.PORT || 5432,
-    port: process.env.DATABASE_PORT || 5432,
-    username: process.env.DATABASE_USER || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'carrynum1',
-    database: process.env.DATABASE_NAME || 'blog-platform',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    server_port: process.env.SERVER_PORT || 5432,
+    port: process.env.POSTGRES_PORT || 5432,
+    username: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRESS_PASSWORD || 'carrynum1',
+    database: process.env.POSTGRES_DB || 'blog-platform',
     migrations: ["dist/migrations/*.js"],
     synchronize: true,
 }));
